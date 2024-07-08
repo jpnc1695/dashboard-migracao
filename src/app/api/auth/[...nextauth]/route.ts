f27a954c7927848f0ab77cb6fc166e9103ad696f
jpnc1695 <joaopaulonunescosta@gmail.com>
2024-07-08 16:58:58 -0300

    alteracoies route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,33 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
-
 const handler = NextAuth({
-providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? ""
-    })
-]
-})
+  providers: [
+      GitHubProvider({
+        clientId: process.env.GITHUB_ID ?? "",
+        clientSecret: process.env.GITHUB_SECRET ?? ""
+      })
+  ]
+  })
+  
+  export {handler as GET, handler as POST}
 
-export {handler as GET, handler as POST}
+// import {authOptionsCredential} from "@/app/lib/auth";
+
+
+// const handler = NextAuth(authOptionsCredential)
+
+
+// export {handler as GET, handler as POST}
+
+
+
+// export const {
+//   handlers:{GET, POST},
+//   auth,
+//   signIn,
+//   signOut,
+// } = NextAuth({
+//   providers:[GitHubProvider )],
+// })
+
